16e3e5d300695e3c19f923807a1006afca3f0d97
manato-takahashi <manato.takahashi86@gmail.com>
2024-04-25 16:43:48 +0900

    add: にゃんこ画像を一通り表示できるところまで実装（仮）

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,30 @@
 import { NextPage } from "next";
+import { useEffect, useState } from "react";
 
 const IndexPage: NextPage = () => {
-    return <div>猫画像予定地</div>;
+    // useStateを使って2つの状態を定義する
+    const [imageUrl, setImageUrl] = useState("");
+    const [loading, setLoading] = useState(true);
+    // マウント時に画像を読み込む宣言
+    useEffect(() => {
+        fetchImage().then((newImage) => {
+            setImageUrl(newImage.url); // 画像URLの状態を更新する
+            setLoading(false); // ローディング状態を更新する
+        });
+    }, []);
+    // ボタンをクリックしたときに画像を読み込む処理
+    const handleClick = async () => {
+        setLoading(true); // 読み込み中フラグを立てる
+        const newImage = await fetchImage();
+        setImageUrl(newImage.url); // 画像URLの状態を更新する
+        setLoading(false); // ローディング状態を更新する
+    };
+    return (
+        <div>
+            <button onClick={handleClick}>他のにゃんこも見る</button>
+            <div>{ loading || <img src={imageUrl} /> }</div>
+        </div>
+    );
 };
 export default IndexPage;
 
