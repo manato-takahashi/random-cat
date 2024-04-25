7c0aed2ff0c8a17986e9c5e73eb037b67a100394
manato-takahashi <manato.takahashi86@gmail.com>
2024-04-25 17:06:47 +0900

    add: 初期画像をサーバーサイドで取得するように実装

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,25 @@
-import { NextPage } from "next";
-import { useEffect, useState } from "react";
+import { GetServerSideProps, NextPage } from "next";
+import { useState } from "react";
 
-const IndexPage: NextPage = () => {
+// getServerSidePropsから渡されるpropsの型
+type Props = {
+    initialImageUrl: string;
+}
+
+// ページコンポーネント関数にpropsを受け取る引数を追加する
+const IndexPage: NextPage<Props> = ({ initialImageUrl }) => {
     // useStateを使って2つの状態を定義する
-    const [imageUrl, setImageUrl] = useState("");
-    const [loading, setLoading] = useState(true);
-    // マウント時に画像を読み込む宣言
-    useEffect(() => {
-        fetchImage().then((newImage) => {
-            setImageUrl(newImage.url); // 画像URLの状態を更新する
-            setLoading(false); // ローディング状態を更新する
-        });
-    }, []);
+    const [imageUrl, setImageUrl] = useState(initialImageUrl); // 初期値を渡す
+    const [loading, setLoading] = useState(false); // 初期状態はfalseにしておく
+
+    // // マウント時に画像を読み込む宣言
+    // useEffect(() => {
+    //     fetchImage().then((newImage) => {
+    //         setImageUrl(newImage.url); // 画像URLの状態を更新する
+    //         setLoading(false); // ローディング状態を更新する
+    //     });
+    // }, []);
+
     // ボタンをクリックしたときに画像を読み込む処理
     const handleClick = async () => {
         setLoading(true); // 読み込み中フラグを立てる
@@ -27,6 +35,16 @@ const IndexPage: NextPage = () => {
     );
 };
 export default IndexPage;
+
+// サーバーサイドで実行する処理
+export const getServerSideProps: GetServerSideProps<Props> = async () => {
+    const image = await fetchImage();
+    return {
+        props: {
+            initialImageUrl: image.url,
+        },
+    };
+};
 
 type Image = {
     url: string;
